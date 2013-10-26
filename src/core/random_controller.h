644b5583c583cc8d5755b434579af9fdbdb01a29
HalalUr <halalur@gmail.com>
2013-10-26 21:10:48 +0200

    Nothin' special. Just removing shit, cos' shit is nogood

--- src/core/random_controller.h
+++ src/core/random_controller.h
@@ -4,7 +4,13 @@
 class RandomController : public Controller
 {
 public:
+	RandomController();
 	virtual void Update(double Seconds);
+protected:
+	virtual void AfterSetActor();
+private:
+	double mCounter;
+	double mHeadingModifier;
 };
 
 #endif//INCLUDED_CORE_RANDOM_CONTROLLER_H
