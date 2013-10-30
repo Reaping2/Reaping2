450a05aa9857630cc0acead13d76f44796106b17
MrPepperoni <aronujvary@gmail.com>
2013-10-30 22:13:45 +0100

    Center the defaultmodel

--- src/render/default_model.cpp
+++ src/render/default_model.cpp
@@ -23,19 +23,19 @@ void DefaultModel::Draw(Actor const& Object)const
 	glScalef(.2f,.2f,0);
 	if(mTexId)
 	{
-         glEnable(GL_TEXTURE_2D);
-		 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		 glEnable(GL_BLEND);
-         glBegin(GL_QUADS);
-         glNormal3f(0.0, 0.0, 1.0);
-		 glColor3f(1.f,1.f,1.f);
-         glTexCoord2d(1, 1); glVertex3f(0.0, 0.0, 0.0);
-         glTexCoord2d(1, 0); glVertex3f(0.0, 1.0, 0.0);
-         glTexCoord2d(0, 0); glVertex3f(1.0, 1.0, 0.0);
-         glTexCoord2d(0, 1); glVertex3f(1.0, 0.0, 0.0);
-         glEnd();
-         glDisable(GL_TEXTURE_2D);
-		 glDisable(GL_BLEND);
+		glEnable(GL_TEXTURE_2D);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glEnable(GL_BLEND);
+		glBegin(GL_QUADS);
+		glNormal3f(0.0, 0.0, 1.0);
+		glColor3f(1.f,1.f,1.f);
+		glTexCoord2d(1, 1); glVertex3f(-0.5f, -0.5f, 0.0f);
+		glTexCoord2d(1, 0); glVertex3f(-0.5f,  0.5f, 0.0f);
+		glTexCoord2d(0, 0); glVertex3f( 0.5f,  0.5f, 0.0f);
+		glTexCoord2d(0, 1); glVertex3f( 0.5f, -0.5f, 0.0f);
+		glEnd();
+		glDisable(GL_TEXTURE_2D);
+		glDisable(GL_BLEND);
 	}
 	else
 	{
