94fa831fb3a4797483e015baa93468a91826c11e
MrPepperoni <aronujvary@gmail.com>
2013-11-23 13:42:42 +0100

    Minor logging changes

--- src/platform/log.cpp
+++ src/platform/log.cpp
@@ -2,13 +2,36 @@
 #include <cstdio>
 #include <cstdarg>
 
-void Logger::Log( char const* format, ... )
+void Logger::Log(int Level,char const* format, ... )
 {
+	if(mDisabledLevels&(1<<Level))
+		return;
 	va_list arg;
 	int done;
 
 	va_start (arg, format);
 	done = vfprintf (stderr, format, arg);
 	va_end (arg);
+}
+
+Logger::Logger()
+: mDisabledLevels(0)
+{
+	AutoFile f(new OsFile("debug.json"));
+	if(!f.get()||!f->IsValid())return;
+	JsonReader Reader(*f.get());
+	if(!Reader.IsValid())return;
+	Json::Value& Root=Reader.GetRoot();
+	if(!Root.isObject())return;
+	Json::Value& DisabledLogs=Root["disabled_logs"];
+	if(!DisabledLogs.isArray())return;
+	const size_t NumDisabledLogs=DisabledLogs.size();
+	for(size_t i=0;i<NumDisabledLogs;++i)
+	{
+		Json::Value& DisLog=DisabledLogs[i];
+		uint32_t Val;
+		if(!GetUInt(DisLog,Val))continue;
+		mDisabledLevels|=(1<<Val);
+	}
 }
 
--- src/platform/log.h
+++ src/platform/log.h
@@ -4,12 +4,21 @@
 class Logger : public Singleton<Logger>
 {
 	friend class Singleton<Logger>;
-	Logger(){}
+	Logger();
+	size_t mDisabledLevels;
 public:
-	void Log(char const* format, ...);
+	void Log(int Level, char const* format, ...);
 };
 
-#define LOG(...) Logger::Get().Log(__VA_ARGS__)
-//#define LOG(...) (void)(__VA_ARGS__)
+#define LOGGING_ENABLED
+#ifdef LOGGING_ENABLED
+#define LOG(...) Logger::Get().Log(0,__VA_ARGS__)
+#define L1(...) Logger::Get().Log(1,__VA_ARGS__)
+#define L2(...) Logger::Get().Log(2,__VA_ARGS__)
+#else
+#define LOG(...) (void)(__VA_ARGS__)
+#define L1(...) (void)(__VA_ARGS__)
+#define L2(...) (void)(__VA_ARGS__)
+#endif//LOGGING_ENABLED
 
 #endif//INCLUDED_PLATFORM_LOG_H
