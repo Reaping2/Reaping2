095bccabb7d344240622e19e3289e41a759251a4
HalalUr <halalur@gmail.com>
2014-10-13 21:08:35 +0200

    Build fix for Win8 vs2k10

--- maketools/build_dependencies.bat
+++ maketools/build_dependencies.bat
@@ -24,9 +24,11 @@ set GENERATOR_NAME=""
 set XIPH_FOLDER=""
 set GLEW_FOLDER=""
 set VCBUILDER=""
+set BOOTSTRAP_TOOLSET=""
 if /i "%MSVS_VER%" == "vs80" (
 	set GENERATOR_NAME="Visual Studio 8 2005"
 	set "B2_TOOLSET=msvc-8.0"
+	set BOOTSTRAP_TOOLSET="vc8"
 	set XIPH_FOLDER="VS2005"
 	set GLEW_FOLDER="vc6"
 	set "VCBUILDER=..\..\VC\vcpackages\vcbuild.exe"
@@ -34,6 +36,7 @@ if /i "%MSVS_VER%" == "vs80" (
 if /i "%MSVS_VER%" == "vs90" (
 	set GENERATOR_NAME="Visual Studio 9 2008"
 	set "B2_TOOLSET=msvc-9.0"
+	set BOOTSTRAP_TOOLSET="vc9"
 	set XIPH_FOLDER="VS2008"
 	set GLEW_FOLDER="vc6"
 	set "VCBUILDER=..\..\VC\vcpackages\vcbuild.exe"
@@ -41,18 +44,21 @@ if /i "%MSVS_VER%" == "vs90" (
 if /i "%MSVS_VER%" == "vs100" (
 	set GENERATOR_NAME="Visual Studio 10"
 	set "B2_TOOLSET=msvc-10.0"
+	set BOOTSTRAP_TOOLSET="vc10"
 	set XIPH_FOLDER="VS2010"
 	set GLEW_FOLDER="vc10"
 )
 if /i "%MSVS_VER%" == "vs110" (
 	set GENERATOR_NAME="Visual Studio 11"
 	set "B2_TOOLSET=msvc-11.0"
+	set BOOTSTRAP_TOOLSET="vc11"
 	set XIPH_FOLDER="VS2010"
 	set GLEW_FOLDER="vc10"
 )
 if /i "%MSVS_VER%" == "vs120" (
 	set GENERATOR_NAME="Visual Studio 12"
 	set "B2_TOOLSET=msvc-12.0"
+	set BOOTSTRAP_TOOLSET="vc12"
 	set XIPH_FOLDER="VS2010"
 	set GLEW_FOLDER="vc10"
 )
@@ -77,12 +83,19 @@ set PWD=%CD%
 set BASEDIR=%PWD%\..
 echo "Working dir: " %PWD%
 echo "Basedir: " %BASEDIR%
-set PATH=%PATH%;%PWD%\cmake-2.8.12-win32-x86\bin;%PWD%\premake4.3
+set PATH=%PATH%;%PWD%\cmake-2.8.12-win32-x86\bin;%PWD%\premake4.3;c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\amd64\
+
+rem ******************************************
+rem here you can junp to any of building stuff
+rem ******************************************
+
 rem goto Build_GLEW
+
 :Build_Boost
 cd %BASEDIR%\deps\boost_1_54_0
-call bootstrap.bat
+call bootstrap.bat %BOOTSTRAP_TOOLSET%
 b2 toolset=%B2_TOOLSET% --with-filesystem --with-exception --with-system --with-thread --with-date_time --with-program_options --with-atomic
+echo "boost built successfully."
 :Build_Glfw
 rd /S /Q %BASEDIR%\deps\glfw-3.0.3\build
 md %BASEDIR%\deps\glfw-3.0.3\build
@@ -90,6 +103,7 @@ cd %BASEDIR%\deps\glfw-3.0.3\build
 %CMAKE_CMD% ..
 %DEVENV% GLFW.sln /Build
 %DEVENV% GLFW.sln /Build "Release"
+echo "glfw built successfully"
 :Build_ZLib
 cd %PWD%
 rd /S /Q %BASEDIR%\deps\zlib-1.2.8\build
@@ -98,6 +112,7 @@ cd %BASEDIR%\deps\zlib-1.2.8\build
 %CMAKE_CMD% ..
 %DEVENV% zlib.sln /Build
 %DEVENV% zlib.sln /Build "Release"
+echo "zlib built successfully"
 copy zconf.h ..
 :Build_LibPng
 rd /S /Q %BASEDIR%\deps\lpng166\build
@@ -110,8 +125,10 @@ set ZLIB_LIBRARY=%BASEDIR%\deps\zlib-1.2.8\build\Debug\zlibd.lib
 set ZLIB_LIBRARY=%BASEDIR%\deps\zlib-1.2.8\build\Release\zlib.lib
 %CMAKE_CMD% -DZLIB_LIBRARY:FILEPATH=%ZLIB_LIBRARY% -DZLIB_INCLUDE_DIR:PATH=%ZLIB_INCLUDE_DIR% ..
 %DEVENV% libpng.sln /Build "Release"
+echo "libpng built successfully"
 cd %BASEDIR%\deps\lpng166
 copy scripts\pnglibconf.h.prebuilt pnglibconf.h
+echo "libpng copied ok"
 :Build_LibOgg
 cd %PWD%
 cd ..\deps\libogg-1.3.1\win32
@@ -119,11 +136,13 @@ cd %XIPH_FOLDER%
 %DEVENV% libogg_static.sln /upgrade
 %DEVENV% libogg_static.sln /Build "Debug"
 %DEVENV% libogg_static.sln /Build "Release"
+echo "libogg1"
 mkdir ..\..\lib
 mkdir ..\..\lib\debug
 mkdir ..\..\lib\release
 copy Win32\Debug\libogg_static.lib ..\..\lib\debug
 copy Win32\Release\libogg_static.lib ..\..\lib\release
+echo "libogg2"
 :Build_LibVorbis
 cd %PWD%
 cd ..\deps\libvorbis-1.3.3\win32
@@ -131,12 +150,14 @@ cd %XIPH_FOLDER%
 %DEVENV% vorbis_static.sln /upgrade
 %DEVENV% vorbis_static.sln /Build "Debug"
 %DEVENV% vorbis_static.sln /Build "Release"
+echo "libvorbis1"
 mkdir ..\..\bin
 mkdir ..\..\bin\lib
 mkdir ..\..\bin\lib\debug
 mkdir ..\..\bin\lib\release
 copy Win32\Debug\*.lib ..\..\bin\lib\debug
 copy Win32\Release\*.lib ..\..\bin\lib\release
+echo "libvorbis2"
 :Build_Portaudio
 cd %PWD%
 cd ..\deps\portaudio_v19
@@ -147,16 +168,21 @@ echo #define PA_WDMKS_NO_KSGUID_LIB >> options_cmake.h
 %DEVENV% portaudio.sln /upgrade
 %DEVENV% portaudio.sln /Build "Debug"
 %DEVENV% portaudio.sln /Build "Release"
+echo "pa1"
 copy options_cmake.h ..\include\pa_options_cmake.h
+echo "pa2"
 cd %PWD%
 :Build_GLEW
+echo "start of building glew"
 cd %PWD%
 cd ..\deps\glew-1.10.0\build
 cd %GLEW_FOLDER%
-if "!VCBUILDER!"=="" (
-	%DEVENV% glew.sln /Build "Debug" 
-	%DEVENV% glew.sln /Build "Release" 
+echo "VCBUILDER: !VCBUILDER! "
+if !VCBUILDER!=="" (
+	%DEVENV% glew.sln /Build "Debug" /project "glew_static"
+	%DEVENV% glew.sln /Build "Release" /project "glew_static" 
 ) else ( 
 	%VCBUILD% glew_static.dsp /Upgrade
 	%VCBUILD% glew_static.vcproj
 )
+echo "end of building glew"
