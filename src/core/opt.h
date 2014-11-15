7bac43522c604402ef206fc0c2944037c9b54019
HalalUr <halalur@gmail.com>
2014-11-15 17:18:55 +0100

    Added Opt support. Replaced GetComponent, to a faster one. ComponentDependent is gone forever. Made the necesery refactorings.

---
+++ src/core/opt.h
@@ -0,0 +1,52 @@
+#ifndef INCLUDED_CORE_OPT_H
+#define INCLUDED_CORE_OPT_H
+template<typename T>
+class Opt
+{
+public:
+	Opt(T* Ptr=NULL);
+	bool IsValid();
+	T* operator->() const;
+	T& operator*() const;
+	T* operator->();
+	T& operator*();
+private:
+	T* mPtr;
+};
+
+template<typename T>
+Opt<T>::Opt(T* Ptr)
+{
+	mPtr=Ptr;
+}
+
+template<typename T>
+bool Opt<T>::IsValid()
+{
+	return mPtr!=NULL;
+}
+
+template<typename T>
+T* Opt<T>::operator->() const
+{
+	return mPtr;
+}
+
+template<typename T>
+T& Opt<T>::operator*() const
+{
+	return *mPtr;
+}
+
+template<typename T>
+T* Opt<T>::operator->()
+{
+	return const_cast<T*>(((const Opt*)this)->operator->());
+}
+
+template<typename T>
+T& Opt<T>::operator*()
+{
+	return const_cast<T&>(((const Opt*)this)->operator*());
+}
+#endif INCLUDED_CORE_OPT_H
