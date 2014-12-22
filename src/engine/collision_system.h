fa6dc6ed256ed4951cf67182a10f901b50ebe159
HalalUr <halalur@gmail.com>
2014-12-22 20:42:48 +0100

    C: linux build fix. backslash in include.

--- src/engine/collision_system.h
+++ src/engine/collision_system.h
@@ -1,10 +1,10 @@
 #ifndef INCLUDED_ENGINE_COLLISION_SYSTEM_H
 #define INCLUDED_ENGINE_COLLISION_SYSTEM_H
 
-#include "core\collision_store.h"
-#include "core\grid.h"
-#include "core\scene.h"
-#include "engine\system.h"
+#include "core/collision_store.h"
+#include "core/grid.h"
+#include "core/scene.h"
+#include "engine/system.h"
 
 namespace engine {
 
