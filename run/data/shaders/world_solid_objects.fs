e44fcb2ba31c0b845d08839dcbf37a01a66eb3ce
Aron Ujvary <aronujvary@gmail.com>
2016-02-02 23:32:41 +0100

    WIP: add a single distortion point for trying out map distortion effect on explosion. todo: render actors with distortion components to texture, use that as texture as displacement input in shader

--- run/data/shaders/world_solid_objects.fs
+++ run/data/shaders/world_solid_objects.fs
@@ -3,7 +3,26 @@
 uniform sampler2D texture;
 smooth in vec2 inTexCoord;
 out vec4 outputColor;
+const vec2 resolution = vec2( 1920.0, 1080.0 );
+const vec2 mouse = vec2( 200.0, 200.0 );
+const float lensSize = 0.1;
 void main()
 {
-    outputColor = vec4(texture2D(texture,inTexCoord.st).xyz,1);
+    vec2 p = inTexCoord;
+    vec2 m = mouse.xy / resolution.xy;
+
+    vec2 d = p - m;
+    float r = sqrt(dot(d, d));
+
+    vec3 col = vec3(0.0, 0.0, 0.0);
+    if( r > lensSize + 0.01 )
+    {
+        col = texture2D(texture, p).xyz;
+    }
+    else
+    {
+        vec2 uv = m + d * r / lensSize;
+        col = texture2D(texture, uv ).xyz;
+    }
+    outputColor = vec4(col,1);
 }
