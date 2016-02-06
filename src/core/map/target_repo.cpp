cb7331ce4235ef7147b1484d0cd44ed00d132e9e
HalalUr <halalur@gmail.com>
2016-02-06 21:57:59 +0100

    A: added some new textures. A: added level4. A: modified pistol sound.

--- src/core/map/target_repo.cpp
+++ src/core/map/target_repo.cpp
@@ -20,6 +20,7 @@ TargetRepo::TargetRepo()
     id=AutoId("wall_small"); mElements.insert(id, new WallTarget(AutoId("wall_small"),AutoId("wall_small"),AutoId("wall_small")));
     id=AutoId("stone_wall"); mElements.insert(id, new WallTarget(id,id,id));
     id=AutoId("water"); mElements.insert(id, new WallTarget(id,id,id));
+    id=AutoId("concrete"); mElements.insert(id, new WallTarget(id,id,id));
     id=AutoId("grass_tile"); mElements.insert(id, new WallTarget(id,id,id));
     id=AutoId("ctf_soldier_spawn_blue"); mElements.insert(id, new CtfSoldierSpawnTarget(id,AutoId("ctf_player"),Team::Blue));
     id=AutoId("ctf_soldier_spawn_red"); mElements.insert(id, new CtfSoldierSpawnTarget(id,AutoId("ctf_player"),Team::Red));
