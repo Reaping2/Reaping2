3e9197cb7755ddef479bfc3735cc43804cbc5099
aujvary <aujvary@cae-engineering.hu>
2016-03-16 15:32:22 +0100

    B: don't use reserved word as argument name, don't return reference to local variable, fix link order

--- src/core/settings.h
+++ src/core/settings.h
@@ -5,14 +5,14 @@
 class Settings : public platform::Singleton<Settings>
 {
 public:
-    std::string GetStr(std::string const& key, std::string const& default="") const;
-    uint32_t GetUInt(std::string const& key, uint32_t default = 0) const;
-    int32_t GetInt(std::string const& key, int32_t default = 0) const;
-    double GetDouble(std::string const& key, double default = 0.0) const;
-    float GetFloat(std::string const& key, float default = 0.0f) const;
-    int32_t GetColor(std::string const& key, int32_t default = 0.0f) const;
-    glm::vec4 GetColor(std::string const& key, glm::vec4 default = glm::vec4(0.0)) const;
-    Json::Value& Resolve(std::string const& key) const;
+    std::string GetStr(std::string const& key, std::string const& Default="") const;
+    uint32_t GetUInt(std::string const& key, uint32_t Default = 0) const;
+    int32_t GetInt(std::string const& key, int32_t Default = 0) const;
+    double GetDouble(std::string const& key, double Default = 0.0) const;
+    float GetFloat(std::string const& key, float Default = 0.0f) const;
+    int32_t GetColor(std::string const& key, int32_t Default = 0.0f) const;
+    glm::vec4 GetColor(std::string const& key, glm::vec4 Default = glm::vec4(0.0)) const;
+    Json::Value Resolve(std::string const& key) const;
 private:
     friend class platform::Singleton<Settings>;
     Settings();
