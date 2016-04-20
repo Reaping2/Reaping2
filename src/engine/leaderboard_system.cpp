609529e9a5ebb0b54ac22b15d432d57b63453bc9
Antal.Ispanovity <weanti@gmail.com>
2016-04-20 17:33:08 +0000

    initial version of using grid in leaderboard

--- src/engine/leaderboard_system.cpp
+++ src/engine/leaderboard_system.cpp
@@ -18,6 +18,18 @@ void LeaderboardSystem::Init()
 {
     mOnMapStart = EventServer<core::MapStartEvent>::Get().Subscribe( boost::bind( &LeaderboardSystem::OnMapStart, this, _1 ) );
     mOnClientDatasChanged = EventServer<engine::ClientDatasChangedEvent>::Get().Subscribe( boost::bind( &LeaderboardSystem::OnClientDatasChanged, this, _1 ) );
+
+    mKDASModels.push_back( new ModelValue( ModelValue::get_string_t( boost::lambda::bind( &LeaderboardSystem::GetColor, this, Team::Blue ) ), "team_blue_color", &mLeaderBoardModel ) );
+    mKDASModels.push_back( new ModelValue( ModelValue::get_string_t( boost::lambda::bind( &LeaderboardSystem::GetColor, this, Team::Red ) ), "team_red_color", &mLeaderBoardModel ) );
+
+    mKDASModels.push_back( new ModelValue( ModelValue::get_string_vec_t( boost::bind(&LeaderboardSystem::BlueNames, this) ), "team_blue_name", &mLeaderBoardModel ) );
+    mKDASModels.push_back( new ModelValue( ModelValue::get_string_vec_t( boost::bind(&LeaderboardSystem::BlueKills, this) ), "team_blue_kill", &mLeaderBoardModel ) );
+    mKDASModels.push_back( new ModelValue( ModelValue::get_string_vec_t( boost::bind(&LeaderboardSystem::BlueDeaths, this) ), "team_blue_death", &mLeaderBoardModel ) );
+    mKDASModels.push_back( new ModelValue( ModelValue::get_string_vec_t( boost::bind(&LeaderboardSystem::BlueScores, this) ), "team_blue_score", &mLeaderBoardModel ) );
+    mKDASModels.push_back( new ModelValue( ModelValue::get_string_vec_t( boost::bind(&LeaderboardSystem::RedNames, this) ), "team_red_name", &mLeaderBoardModel ) );
+    mKDASModels.push_back( new ModelValue( ModelValue::get_string_vec_t( boost::bind(&LeaderboardSystem::RedKills, this) ), "team_red_kill", &mLeaderBoardModel ) );
+    mKDASModels.push_back( new ModelValue( ModelValue::get_string_vec_t( boost::bind(&LeaderboardSystem::RedDeaths, this) ), "team_red_death", &mLeaderBoardModel ) );
+    mKDASModels.push_back( new ModelValue( ModelValue::get_string_vec_t( boost::bind(&LeaderboardSystem::RedScores, this) ), "team_red_score", &mLeaderBoardModel ) );
 }
 
 
@@ -37,67 +49,86 @@ void LeaderboardSystem::OnClientDatasChanged( engine::ClientDatasChangedEvent co
 
 void LeaderboardSystem::SetModels()
 {
-    mKDASModels.clear();
-    int32_t blueCounter = 0;
-    int32_t redCounter = 0;
-    for ( ::ctf::ProgramState::ClientDatas_t::iterator i = mCtfProgramState.mClientDatas.begin(), e = mCtfProgramState.mClientDatas.end(); i != e; ++i )
+    mBlueNames.clear();
+    mBlueKills.clear();
+    mBlueDeaths.clear();
+    mBlueScores.clear();
+    mRedNames.clear();
+    mRedKills.clear();
+    mRedDeaths.clear();
+    mRedScores.clear();
+    for ( auto it = mCtfProgramState.mClientDatas.begin(), e = mCtfProgramState.mClientDatas.end(); it != e; ++it )
     {
-        ::ctf::ClientData& ctfClientData = *i;
+        ::ctf::ClientData& ctfClientData = *it;
         Opt<core::ClientData> clientData = mProgramState.FindClientDataByClientId( ctfClientData.mClientId );
         if ( !clientData.IsValid() )
         {
             continue;
         }
-        std::string modelString = "team";
+        std::string name = clientData->mClientName;
+        std::string kill = std::to_string( clientData->mKill );
+        std::string death = std::to_string( clientData->mDeath );
+        std::string score = std::to_string( clientData->mScore );
         if ( ctfClientData.mTeam == Team::Blue )
         {
-            ++blueCounter;
-            modelString = modelString + "_blue_" + boost::lexical_cast<std::string>( blueCounter ) + "_";
+            mBlueNames.push_back( name );
+            mBlueKills.push_back( kill );
+            mBlueDeaths.push_back( death );
+            mBlueScores.push_back( score );
         }
         else
         {
-            ++redCounter;
-            modelString = modelString + "_red_" + boost::lexical_cast<std::string>( redCounter ) + "_";
+            mRedNames.push_back( name );
+            mRedKills.push_back( kill );
+            mRedDeaths.push_back( death );
+            mRedScores.push_back( score );
         }
-
-        mKDASModels.push_back( new ModelValue( ModelValue::get_string_t( boost::lambda::bind( &LeaderboardSystem::GetClientName, boost::cref( *clientData.Get() ) ) ), modelString + "name", &mLeaderBoardModel ) );
-        mKDASModels.push_back( new ModelValue( ModelValue::get_int_t( boost::lambda::bind( &LeaderboardSystem::GetKill, boost::cref( *clientData.Get() ) ) ), modelString + "kill", &mLeaderBoardModel ) );
-        mKDASModels.push_back( new ModelValue( ModelValue::get_int_t( boost::lambda::bind( &LeaderboardSystem::GetDeath, boost::cref( *clientData.Get() ) ) ), modelString + "death", &mLeaderBoardModel ) );
-        mKDASModels.push_back( new ModelValue( ModelValue::get_int_t( boost::lambda::bind( &LeaderboardSystem::GetAssist, boost::cref( *clientData.Get() ) ) ), modelString + "assist", &mLeaderBoardModel ) );
-        mKDASModels.push_back( new ModelValue( ModelValue::get_int_t( boost::lambda::bind( &LeaderboardSystem::GetScore, boost::cref( *clientData.Get() ) ) ), modelString + "score", &mLeaderBoardModel ) );
     }
-    mKDASModels.push_back( new ModelValue( ModelValue::get_string_t( boost::lambda::bind( &LeaderboardSystem::GetColor, this, Team::Blue ) ), "team_blue_color", &mLeaderBoardModel ) );
-    mKDASModels.push_back( new ModelValue( ModelValue::get_string_t( boost::lambda::bind( &LeaderboardSystem::GetColor, this, Team::Red ) ), "team_red_color", &mLeaderBoardModel ) );
-}
-
-std::string const& LeaderboardSystem::GetClientName( core::ClientData const& clientData )
-{
-    return clientData.mClientName;
-}
-
-int32_t const& LeaderboardSystem::GetDeath( core::ClientData const& clientData )
-{
-    return clientData.mDeath;
-}
-
-int32_t const& LeaderboardSystem::GetAssist( core::ClientData const& clientData )
-{
-    return clientData.mAssist;
-}
-
-int32_t const& LeaderboardSystem::GetScore( core::ClientData const& clientData )
-{
-    return clientData.mScore;
-}
-
-int32_t const& LeaderboardSystem::GetKill( core::ClientData const& clientData )
-{
-    return clientData.mKill;
 }
 
 std::string const& LeaderboardSystem::GetColor( Team::Type team ) const
 {
     return team == Team::Blue ? mBlueColor : mRedColor;
+}
+
+std::vector<std::string> LeaderboardSystem::BlueNames()
+{
+    return mBlueNames;
+}
+
+std::vector<std::string> LeaderboardSystem::BlueKills()
+{
+    return mBlueKills;
+}
+
+std::vector<std::string> LeaderboardSystem::BlueDeaths()
+{
+    return mBlueDeaths;
+}
+
+std::vector<std::string> LeaderboardSystem::BlueScores()
+{
+    return mBlueScores;
+}
+
+std::vector<std::string> LeaderboardSystem::RedNames()
+{
+    return mRedNames;
+}
+
+std::vector<std::string> LeaderboardSystem::RedKills()
+{
+    return mRedKills;
+}
+
+std::vector<std::string> LeaderboardSystem::RedDeaths()
+{
+    return mRedDeaths;
+}
+
+std::vector<std::string> LeaderboardSystem::RedScores()
+{
+    return mRedScores;
 }
 
 
