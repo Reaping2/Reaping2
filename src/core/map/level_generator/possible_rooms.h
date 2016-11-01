db326a0909b315c52c36fe380ff1cfc1b3580219
HalalUr <halalur@gmail.com>
2016-11-01 18:54:20 +0100

    Added room entrance check and room replace. Further improvement will be a can't swap property and a room entrance check at room placement.

--- src/core/map/level_generator/possible_rooms.h
+++ src/core/map/level_generator/possible_rooms.h
@@ -14,10 +14,16 @@ public:
     typedef std::vector<int32_t> PossibleRoomIds_t;
     PossibleRoomIds_t const& GetRoomIds();
     PossibleRoomIds_t GetRoomIdsByProperty( RoomProperty::Type prop, bool shuffled = true );
+    PossibleRoomIds_t GetRoomIdsFiltered( RoomDesc const& roomDesc, int32_t flags, bool shuffled = true );
 private:
+    // depending on room weight there can be more than one roomId of the same Id.
     PossibleRoomIds_t mRoomIds;
+    // unique (and shuffled) ids
+    PossibleRoomIds_t mUniqueRoomIds;
     RoomRepo& mRoomRepo;
+    // don't forget to update mUniqueRoomIds!
     void AddPossibleRoom( int32_t roomId, int32_t possibility );
+    PossibleRoomIds_t MakeRoomeIdsUnique(PossibleRoomIds_t const& roomIds);
 };
 
 } // namespace map
