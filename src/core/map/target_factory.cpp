a205e4bb1f2af9411d61c7ab4b61c0a391ec5c38
Antal.Ispanovity <weanti@gmail.com>
2016-12-13 15:35:46 +0100

    simplify target factory

--- src/core/map/target_factory.cpp
+++ src/core/map/target_factory.cpp
@@ -15,30 +15,15 @@ TargetFactory::TargetFactory()
     Bind( AutoId("default_i_target"), &CreateITarget<DefaultTarget> );
     SetDefault( AutoId("default_i_target") );
     // wall types
-    Bind( AutoId("wall"), &CreateITarget<WallTarget>);
-    Bind( AutoId("wall_small"), &CreateITarget<WallTarget>);
-    Bind( AutoId("stone_wall"), &CreateITarget<WallTarget>);
-    Bind( AutoId("water"), &CreateITarget<WallTarget>);
-    Bind( AutoId("concrete"), &CreateITarget<WallTarget>);
-    Bind( AutoId("rogue_grass200"), &CreateITarget<WallTarget>);
-    Bind( AutoId("rogue_wall50_background"), &CreateITarget<WallTarget>);
-    Bind( AutoId("rogue_wall100_background"), &CreateITarget<WallTarget>);
-    Bind( AutoId("rogue_wall100"), &CreateITarget<WallTarget>);
-    Bind( AutoId("rogue_wall50"), &CreateITarget<WallTarget>);
-    Bind( AutoId("guard"), &CreateITarget<WallTarget>);
-    Bind( AutoId("skull"), &CreateITarget<WallTarget>);
-    Bind( AutoId("grass_tile"), &CreateITarget<WallTarget>);
-    Bind( AutoId("end"), &CreateITarget<WallTarget>);
+    Bind( AutoId("wall_target"), &CreateITarget<WallTarget>);
     // ctf soldier spawn types
-    Bind( AutoId("ctf_soldier_spawn_blue"), &CreateITarget<CtfSoldierSpawnTarget>);
-    Bind( AutoId("ctf_soldier_spawn_red"), &CreateITarget<CtfSoldierSpawnTarget>);
+    Bind( AutoId("ctf_soldier_spawn_target"), &CreateITarget<CtfSoldierSpawnTarget>);
     // soldier spawn types
-    Bind( AutoId("soldier_spawn"), &CreateITarget<SoldierSpawnTarget>);
+    Bind( AutoId("soldier_spawn_target"), &CreateITarget<SoldierSpawnTarget>);
     // pickup types
     Bind( AutoId("revive_pickup"), &CreateITarget<PickupTarget>);
     // flag spawn types
-    Bind( AutoId( "ctf_flag_spawn_blue" ), &CreateITarget<FlagSpawnTarget>);
-    Bind( AutoId( "ctf_flag_spawn_red" ), &CreateITarget<FlagSpawnTarget>);
+    Bind( AutoId( "flag_spawn_target" ), &CreateITarget<FlagSpawnTarget>);
 
 }
 
